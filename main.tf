5aaa8f7a5b1121d55d8da79cc1c02b5ef7fed164
Ernesto Juarez <ernestojuarezgutierrez03@gmail.com>
2025-12-11 07:32:39 -0400

    Hola mundo

--- main.tf
+++ main.tf
@@ -25,6 +25,44 @@ data "aws_ami" "amazon_linux" {
   owners = ["amazon"]
 }
 
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-datasource-sg-2"
+  vpc_id      = data.aws_vpc.selected.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-datasource-sg-2"
+  }
+}
+
 # 4. Crear EC2 usando esos data sources
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon_linux.id
